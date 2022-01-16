7e10b674cce8d23261c483c6bd357b6ed03c2638
janneandrea16 <85207772+janneandrea16@users.noreply.github.com>
2022-01-15 23:56:53 -0500

    Next_move is now make_next_move

--- tic-toc.py
+++ tic-toc.py
@@ -1,9 +1,4 @@
 '''
-Tic-Tac-Toe: A Solution
-Author: Bro. Manley
-'''
-
-'''
 Tic-Tac-Toe is a game in which two players seek in alternate turns to complete a row, a column, or a diagonal with either three x's or three o's drawn in the spaces of a grid of nine squares.
 '''
 
@@ -17,7 +12,7 @@ def main():
     #If all nine squares are full and neither player has three in a row, the game ends in a draw.
     while not (has_winner(board) or is_a_draw(board)):
         display_board(board)
-        make_move(player, board)
+        make_next_move(player, board)
         player = next_player(player)
     display_board(board)
     print("You did very well! Thanks for playing :) ") 
@@ -56,7 +51,7 @@ def has_winner(board):
         board[0] == board[4] == board[8] or
         board[2] == board[4] == board[6])
 
-def make_move(player,board):
+def make_next_move(player,board):
     square = int(input(f"{player}'s turn to choose (1-9):"))
     board[square - 1]=player
 
